1656c68d248088cf68411873d502cc08389ae806
Alex Brandt <alunduil@alunduil.com>
2013-08-11 22:01:44 -0500

    Add holland manifest for testing.

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,14 +1,46 @@
 node /holland.*/ inherits default {
-  package { 'app-backup/holland':
-    ensure => 'latest',
-    require => Exec['keyword holland'],
-  }
-
   exec { 'keyword holland':
-    command => 'echo app-backup/holland ~amd64 >> package.accept_keywords',
+    command => 'echo app-backup/holland"*" ~amd64 >> package.accept_keywords',
     cwd => '/etc/portage',
     unless => 'grep holland package.accept_keywords',
     path => [ '/bin', '/usr/bin' ],
+  }
+
+  exec { 'eix-sync':
+    path => [ '/bin', '/usr/bin' ],
+  }
+
+  package { 'app-backup/holland':
+    ensure => 'latest',
+    require => [ 
+      Exec['keyword holland'],
+      Exec['eix-sync'],
+      ],
+    before => Exec['config mysql'],
+  }
+
+  exec { 'config mysql':
+    command => 'yes password | emerge --config mysql',
+    unless => 'test -d /var/lib/mysql/mysql',
+    path => [ '/bin', '/usr/bin' ],
+    require => Package['app-backup/holland'],
+  }
+
+  file { '/root/.my.cnf':
+    content => "
+               [client]\n
+               user     = root\n
+               password = password\n
+               ",
+    group => 'root',
+    mode => 0600,
+    owner => 'root',
+  }
+
+  service { 'mysql':
+    ensure => 'running',
+    hasrestart => true,
+    require => Exec['config mysql'],
   }
 }  
 
